e87b053c57ce076aaeec74f16ad958bc85d59564
Roberto Margain <margain.roberto@gmail.com>
2022-05-10 22:02:56 -0500

    themes

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,43 @@
-import { AuthProvider } from '../lib/auth'
-import '../styles/globals.css'
+import {ChakraProvider, CSSReset } from "@chakra-ui/react";
+import { AuthProvider } from "@/lib/auth";
+import theme  from "@/styles/theme";
+import { Global, css} from "@emotion/react";
+import Head from "next/head";
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-  <AuthProvider>
-    <Component {...pageProps} />
-  </AuthProvider>
-  )
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        {/* <GlobalStyle /> */}
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
